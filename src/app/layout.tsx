279a1c704dad30ce5586185e8749bab394de71ed
Aldo Miralles <alditomiralles3@gmail.com>
2024-07-13 16:59:11 -0600

    adding favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Cooking by Lea",
   description: "Cooking by Lea est un blog de recettes proposant une grande variété de plats, y compris des options saines, pour satisfaire tous les goûts et régimes alimentaires. Explorez des guides étape par étape et des images vibrantes pour inspirer votre prochaine création culinaire.",
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png']
+  }
 };
 
 export default function RootLayout({
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="bg-white shadow dark:bg-gray-900">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <Link href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <Link
+            href="/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
             <Image
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
+              src="/cooking-by-Léa.png"
+              className=""
               alt="Flowbite Logo"
-              width={32}
-              height={32}
+              width={80}
+              height={80}
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Cooking by Lea
             </span>
           </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -44,8 +47,8 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 text-center dark:text-gray-400">
-          © 2024{' '}
-          <Link href="https://flowbite.com/" className="hover:underline">
+          © 2024{" "}
+          <Link href="/" className="hover:underline">
             Cooking by Lea
           </Link>
           . Made with ❤️ by Aldo.
